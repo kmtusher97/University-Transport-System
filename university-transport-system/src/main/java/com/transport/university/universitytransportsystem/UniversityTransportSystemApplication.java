50c7e9d007c8fd24c05dba4f3232e7d7271239f8
kmtusher97 <kmtusher97@gmail.com>
2020-03-11 18:46:09 +0600

    Create Spring Boot project and add database

---
+++ university-transport-system/src/main/java/com/transport/university/universitytransportsystem/UniversityTransportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transport.university.universitytransportsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityTransportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityTransportSystemApplication.class, args);
+	}
+
+}
---
+++ university-transport-system/src/test/java/com/transport/university/universitytransportsystem/UniversityTransportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.university.universitytransportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityTransportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
