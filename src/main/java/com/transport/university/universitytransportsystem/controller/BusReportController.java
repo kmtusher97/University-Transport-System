e18d61de1a629b550d9cccd504ada2fe3639b915
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-03-20 06:27:34 +0600

    Bus Report Services

---
+++ src/main/java/com/transport/university/universitytransportsystem/controller/BusReportController.java
@@ -0,0 +1,46 @@
+package com.transport.university.universitytransportsystem.controller;
+
+import com.transport.university.universitytransportsystem.model.BusReport;
+import com.transport.university.universitytransportsystem.service.BusReportServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/busReport")
+public class BusReportController {
+
+    @Autowired
+    private BusReportServices busReportServices;
+
+    @GetMapping("/getAll")
+    public List<BusReport> getAllBusReport() {
+        return busReportServices.getAllBusReport();
+    }
+
+    @PostMapping("/add")
+    public BusReport addBusReport(@RequestBody BusReport busReport) {
+        return busReportServices.addBusReport(busReport);
+    }
+
+    @GetMapping("/getByBusId/{busId}")
+    public List<BusReport> getBusReportByBusId(@PathVariable("busId") Integer busId) {
+        return busReportServices.getBusReportByBusId(busId);
+    }
+
+    @PostMapping("/getByDateInterval")
+    public List<BusReport> getBusReportsByDate(@RequestBody List<Date> dates) {
+        if (dates == null || dates.size() != 2) return null;
+        Date startDate = dates.get(0);
+        Date endDate = dates.get(1);
+        if (startDate.compareTo(endDate) == 1) return null;
+        return busReportServices.getBusReportsByDateInterval(startDate, endDate);
+    }
+
+    @DeleteMapping("/delete/{reportId}")
+    public void deleteReportById(@PathVariable("reportId") Long reportId) {
+        busReportServices.deleteByBusReportId(reportId);
+    }
+}
