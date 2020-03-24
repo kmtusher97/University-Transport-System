08d9b9e7adfac44e0c58cc1aa99769bca1dbc84e
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-03-25 03:24:23 +0600

    Get nth 30 announcemetns feature added

--- src/main/java/com/transport/university/universitytransportsystem/repository/AnnouncementRepo.java
+++ src/main/java/com/transport/university/universitytransportsystem/repository/AnnouncementRepo.java
@@ -2,6 +2,12 @@ package com.transport.university.universitytransportsystem.repository;
 
 import com.transport.university.universitytransportsystem.model.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AnnouncementRepo extends JpaRepository<Announcement, Long> {
+
+    @Query(value = "SELECT * FROM announcement LIMIT 30 OFFSET ?1", nativeQuery = true)
+    List<Announcement> getNth30AnnouncementsFromBack(long fromRow);
 }
