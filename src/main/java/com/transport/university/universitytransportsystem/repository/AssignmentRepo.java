844d54c9de8dc6ad22a5d2d2d82120569389d813
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-04-05 22:17:57 +0600

    return latest 30 records and return after reversing

--- src/main/java/com/transport/university/universitytransportsystem/repository/AssignmentRepo.java
+++ src/main/java/com/transport/university/universitytransportsystem/repository/AssignmentRepo.java
@@ -13,6 +13,6 @@ public interface AssignmentRepo extends JpaRepository<Assignment, Long> {
     @Query("SELECT a FROM Assignment a WHERE a.date >= ?1 AND a.date <= ?2")
     List<Assignment> getAllByDateRange(Date startDate, Date endDate);
 
-    @Query(value = "SELECT * FROM assignment LIMIT 10 OFFSET ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM assignment LIMIT 30 OFFSET ?1", nativeQuery = true)
     List<Assignment> getNth30SchedulesFromLast(Long fromRow);
 }
