bc2ebbb8ba750ad621a5a33a1e6511445a6fc5a8
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-04-29 01:33:27 +0600

    Find user by email(username) for userDetailsService

--- src/main/java/com/transport/university/universitytransportsystem/repository/UserRepo.java
+++ src/main/java/com/transport/university/universitytransportsystem/repository/UserRepo.java
@@ -4,4 +4,5 @@ import com.transport.university.universitytransportsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
