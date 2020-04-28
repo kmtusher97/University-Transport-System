6bbc43c95c36fe678ec4445ab94975a2cf654401
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-04-29 04:22:16 +0600

    Make GLOBAL apis public

--- src/main/java/com/transport/university/universitytransportsystem/security/SecurityConfig.java
+++ src/main/java/com/transport/university/universitytransportsystem/security/SecurityConfig.java
@@ -44,7 +44,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .authorizeRequests().antMatchers(
+                "/authenticate",
+                "/api/**/GLOBAL/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
