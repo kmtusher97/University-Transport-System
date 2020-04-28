02bc1760d53fe3a4fa887b4cd1d5526e3c2033a3
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-04-29 05:02:07 +0600

    Add isBlocked() and user roles to userDetails

--- src/main/java/com/transport/university/universitytransportsystem/service/CustomUserDetailsService.java
+++ src/main/java/com/transport/university/universitytransportsystem/service/CustomUserDetailsService.java
@@ -1,8 +1,11 @@
 package com.transport.university.universitytransportsystem.service;
 
+import com.transport.university.universitytransportsystem.model.Role;
 import com.transport.university.universitytransportsystem.model.User;
 import com.transport.university.universitytransportsystem.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,6 +27,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         return userRepo.findByEmail(email);
     }
 
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return authorities;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User userFromDB = getUserByEmail(email);
@@ -31,7 +44,11 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 userFromDB.getEmail(),
                 userFromDB.getPassword(),
-                new ArrayList<>()
+                !userFromDB.getIsBlocked(),
+                true,
+                true,
+                true,
+                getAuthorities(userFromDB.getRoles())
         );
 
     }
