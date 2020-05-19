96e1e90bd4c755eda4636f2dad6942cea8bb1d6e
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-05-19 15:43:20 +0600

    Viwe added

---
+++ src/main/resources/static/precache-manifest.c2c60608ef2c68274a43b0dacf7c9c61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbd76b627f211def883221117b6f566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f99ab68ba4ac753f722",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2fbd5081a8a348fb83b5",
+    "url": "/static/css/main.e3778778.chunk.css"
+  },
+  {
+    "revision": "4f99ab68ba4ac753f722",
+    "url": "/static/js/2.0724cf59.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0724cf59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbd5081a8a348fb83b5",
+    "url": "/static/js/main.febebfaf.chunk.js"
+  },
+  {
+    "revision": "b37768e53267d05b6d88",
+    "url": "/static/js/runtime-main.7d44394e.js"
+  }
+]);
