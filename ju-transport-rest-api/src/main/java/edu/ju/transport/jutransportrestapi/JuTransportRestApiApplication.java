7f60aaa7160cbf0b74843333be2d965767397b17
kmtusher97 <kmtusher97@gmail.com>
2021-03-30 20:49:21 +0600

    Recreate rest api project for version 2

---
+++ ju-transport-rest-api/src/main/java/edu/ju/transport/jutransportrestapi/JuTransportRestApiApplication.java
@@ -0,0 +1,13 @@
+package edu.ju.transport.jutransportrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuTransportRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuTransportRestApiApplication.class, args);
+    }
+
+}
