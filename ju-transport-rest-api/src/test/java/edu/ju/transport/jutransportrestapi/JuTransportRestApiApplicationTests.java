000f4849e8e739a3c81581b50596809a763a209c
kmtusher97 <kmtusher97@gmail.com>
2021-03-30 20:49:21 +0600

    Recreate rest api project for version 2

---
+++ ju-transport-rest-api/src/test/java/edu/ju/transport/jutransportrestapi/JuTransportRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ju.transport.jutransportrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuTransportRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
