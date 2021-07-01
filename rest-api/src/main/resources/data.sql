808b6b07bffd4b68ba26ab2529ff8a0439aad27f
kmtusher97 <kmtusher97@gmail.com>
2021-07-02 04:37:50 +0600

    Add database initializer data

---
+++ rest-api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO role (role_id, role_name) VALUES ('', 'ROLE_ADMIN');
+INSERT INTO role (role_id, role_name) VALUES ('', 'ROLE_USER');
+INSERT INTO role (role_id, role_name) VALUES ('', 'ROLE_DRIVER');
+INSERT INTO role (role_id, role_name) VALUES ('', 'ROLE_STUFF');
